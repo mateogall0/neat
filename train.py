37986e50322eb5922da73d706877e99933145705
gallit0 <mateogesede@gmail.com>
2025-04-02 15:25:54 -0300

    Ready to modify data for a harder environment

--- train.py
+++ train.py
@@ -2,9 +2,19 @@
 from eval import eval_genomes
 from data import x, y
 import neat
+import matplotlib.pyplot as plt
 
 
-def run(config_file):
+def plot_train(gens: list, avg_fitness: list):
+    plt.plot(gens, avg_fitness, label='Average Fitness')
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness')
+    plt.title('NEAT Algorithm Fitness Progress')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def run(config_file, plot=True):
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_file)
@@ -15,8 +25,20 @@ def run(config_file):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    winner = p.run(eval_genomes, 300)
+    avg_fitness = []
 
+    num_gens = 16384
+    step = 1024
+    for generation in range(0, num_gens, step):
+        p.run(eval_genomes, step)
+        current_avg_fitness = stats.get_fitness_mean()[-1]
+        avg_fitness.append(current_avg_fitness)
+        print(f"Generation {generation}: Avg Fitness = {current_avg_fitness}")
+
+    gens = list(range(0, num_gens, step))
+    plot_train(gens, avg_fitness)
+
+    winner = stats.best_genome()
     print('\nBest genome:\n{!s}'.format(winner))
 
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
