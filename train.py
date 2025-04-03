1f9bb84e92808d9dc4cee4610e9fd278711c6c09
gallit0 <mateogesede@gmail.com>
2025-04-03 09:50:45 -0300

    Add feature to plot genome size

--- train.py
+++ train.py
@@ -5,13 +5,22 @@ import neat
 import matplotlib.pyplot as plt
 
 
-def plot_train(num_gens: int, avg_fitness: list):
+def plot_train(num_gens: int, avg_fitness: list, genome_size: list):
+    plt.subplot(2, 1, 1)
     plt.plot(list(range(num_gens)), avg_fitness, label='Average Fitness')
     plt.xlabel('Generation')
     plt.ylabel('Fitness')
     plt.title('NEAT Algorithm Fitness Progress')
     plt.legend()
     plt.grid(True)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(list(range(num_gens)), genome_size, label='Best Genome Size')
+    plt.xlabel('Generation')
+    plt.ylabel('Size')
+    plt.title('Best Genome Size Progress')
+    plt.legend()
+    plt.tight_layout()
     plt.show()
 
 def run(config_file, plot=True):
@@ -26,14 +35,17 @@ def run(config_file, plot=True):
     p.add_reporter(stats)
 
     avg_fitness = []
+    genome_size = []
 
     num_gens = 8
     step = 1
     for generation in range(0, num_gens, step):
         p.run(eval_genomes, step)
         avg_fitness.append(stats.get_fitness_mean()[-1:])
+        current_best = stats.best_genome()
+        genome_size.append(len(current_best.connections) + len(current_best.nodes))
 
-    plot_train(len(avg_fitness), avg_fitness)
+    plot_train(len(avg_fitness), avg_fitness, genome_size)
 
     winner = stats.best_genome()
     print('\nBest genome:\n{!s}'.format(winner))
