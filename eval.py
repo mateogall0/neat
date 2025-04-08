02317bfb33f991ae6865e95a77894b70121ac0fc
gallit0 <mateogesede@gmail.com>
2025-04-08 15:09:15 -0300

    wip: training with wine

--- eval.py
+++ eval.py
@@ -8,10 +8,25 @@ def squared_error_fit(xi, yi):
     yi = float(yi)
     return (xi - yi) ** 2
 
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=-1, keepdims=True)
+
+def cross_entropy_loss(y_pred, y_true):
+    y_true_one_hot = np.zeros_like(y_pred)
+    y_true_one_hot[int(y_true)] = 1
+
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+
+    return -np.sum(y_true_one_hot * np.log(y_pred))
+
 def eval_genomes(genomes, config):
     for _, g in genomes:
         net = neat.nn.FeedForwardNetwork.create(g, config)
         g.fitness = 4.0
         for xi, yi in zip(x, y):
             output = net.activate(xi)
-            g.fitness -= squared_error_fit(np.argmax(output), yi)
+            Y = softmax(output)
+            #g.fitness -= squared_error_fit(np.argmax(Y), yi)
+            g.fitness -= cross_entropy_loss(Y, yi)
--- train.py
+++ train.py
@@ -135,8 +135,8 @@ def run(config_file, plot=True):
         best_val_fitness = []
         validation_amount = []
 
-    num_gens = 4096
-    step = 256
+    num_gens = 128
+    step = 16
     for generation in range(0, num_gens, step):
         p.run(eval_genomes, step)
         if plot:
